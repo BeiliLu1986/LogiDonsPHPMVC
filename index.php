659bcfd81a42c4ae0488fea768c44944781d7008
afsaneh <afsanehm209@gmail.com>
2018-09-02 22:53:21 -0400

    modifier index

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
-// -- Contrôleur frontal --
+// -- Controleur frontal --
 require_once('./controleur/ActionBuilder.class.php');
-require_once('./controleur/RequirePRGAction.interface.php');
-$action = NULL;
-$vue = NULL;
 if (ISSET($_REQUEST["action"]))
 	{
+		$vue = ActionBuilder::getAction($_REQUEST["action"])->execute();
+		/*
+		Ou :
 		$action = ActionBuilder::getAction($_REQUEST["action"]);
 		$vue = $action->execute();
+		*/
 	}
 else	
 	{
 		$action = ActionBuilder::getAction("");
 		$vue = $action->execute();
 	}
-if ($action instanceof RequirePRGAction) {
-	
-	header("Location: ?action=".$vue);
-}
-else {
-	include_once('vues/'.$vue.'.php');
-}
+// On affiche la page (vue)
+include_once('vues/main.php');
+
 ?>
