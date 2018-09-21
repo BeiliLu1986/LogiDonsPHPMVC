274284f3f955448bf1fd674723ba5f075220e6e9
Gulmira <36450002+Imgumi@users.noreply.github.com>
2018-09-21 19:26:06 -0300

    Add files via upload
    ajoutes les methodes pour inserer empl etc

--- model/UserDAO.class.php
+++ model/UserDAO.class.php
@@ -30,7 +30,7 @@ class UserDAO
 		$pstmt->closeCursor();
 		return null;
 	}
-        //methode pour trouver tous employes
+        //methode pour trouver tous les employes
         public static function findAllEmpls(){
         try {
 			$liste = new Liste();
@@ -63,6 +63,15 @@ class UserDAO
 		if ($result)
 		{
 			$u->setId_user($result->id_user);
+                        $u->setNom($result->nom);
+                        $u->setPrenom($result->prenom);
+                        $u->setCourriel($result->courriel);
+                        $u->setTelephone($result->telephone);
+                        $u->setVille($result->ville);
+                        $u->setProvince($result->province);
+                        $u->setCode_postale($result->code_postale);
+                        $u->setAdresse($result->adresse);
+                        $u->setPassword($result->password);
 			$pstmt->closeCursor();
 			return $u;
 		}
@@ -119,6 +128,43 @@ class UserDAO
             }
             catch (PDOException $ex){
             }     
-	}       
+	} 
+        
+    public static function addEmploye($newUser)
+	{
+		$db = Database::getInstance();
+		try {
+                $pstmt = $db->prepare("INSERT INTO user (id_user ,nom,prenom,type_user,courriel,telephone,"
+                        . "ville,province,code_postale,adresse,password)".
+                                                  " VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:m,:y)");
+                $n = $pstmt->execute(array(':a' => $newUser->getId_user(),':b' => $newUser->getNom(),
+                    ':c' => $newUser->getPrenom(),':d' => 'emp',':e' => $newUser->getCourriel(),
+                    ':f' => $newUser->getTelephone(),':g' => $newUser->getVille(),
+                    ':h' => $newUser->getProvince(),':i' => $newUser->getCode_postale(),':m' => $newUser->getAdresse(),':y' => $newUser->getPassword()));
+
+                $pstmt->closeCursor();
+                $pstmt = NULL;
+            }
+            catch (PDOException $ex){
+            }     
+	}
+        
+        public static function updateUser($x)
+	{
+		$request = "UPDATE user SET nom = '".$x->getNom()."', prenom = '".$x->getPrenom()."', "
+                        . "courriel = '".$x->getCourriel()."', telephone = '".$x->getTelephone()."', ville = '".$x->getVille()."', "
+                        . "province = '".$x->getProvince()."', code_postale = '".$x->getCode_postale()."', adresse = '".$x->getAdresse()."', password = '".$x->getPassword()."'"
+                        . " WHERE id_user = '".$x->getId_user()."'";
+		try
+		{
+			$db = Database::getInstance();
+			return $db->exec($request);
+		}
+		catch(PDOException $e)
+		{
+			throw $e;
+		}
+	}
+        
 }      
 ?>
