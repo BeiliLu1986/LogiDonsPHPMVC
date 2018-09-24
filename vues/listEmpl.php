96f1da206c0a6ff9c25c1e565aff103337f4ea43
BeiLi <bellyluis@gmail.com>
2018-09-23 20:41:10 -0400

    bootstrap menu action employes

--- vues/listEmpl.php
+++ vues/listEmpl.php
@@ -1,3 +1,41 @@
-<h1>Liste des employes</h1>
-<a href="?action=ajoutEmpl">Ajouter un employe</a><br>
-<a href="?action=modifEmpl">Modifier un employe</a>
+<link rel="stylesheet" type="text/css" href="./css/listEmpl.css">
+<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4">
+					<h1 class="display-4">Employes</h1>
+					<hr>
+				</div>
+				<div class="col-md-4">
+				</div>
+			</div>
+		</div>
+</div>
+<div class="row">
+	<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<div class="row" style=" background-color: #eee;">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+               		<div class="box-part text-center">
+                        <i class="fa fa-user fa-3x" aria-hidden="true"></i>
+                        <div class="title">
+							<h4>Ajouter</h4>
+						</div>
+						<a class="link" href="?action=ajoutEmpl">Ajouter un employe</a><br>
+                    </div>
+				</div>	 
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="box-part text-center">
+					<i class="fa fa-pencil fa-3x" aria-hidden="true"></i>
+					<div class="title">
+						<h4>Modifier</h4>
+					</div>
+                    <a class="link" href="?action=modifEmpl">Modifier un employe</a>
+                </div>
+				</div>	
+			</div>
+		</div>
+		<div class="col-md-4"><br></div>
+</div>
