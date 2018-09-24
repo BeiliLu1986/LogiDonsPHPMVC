7628578a014e3e9f23fea6a330d7032841eeae9d
BeiLi <bellyluis@gmail.com>
2018-09-24 09:27:01 -0400

    change general

--- vues/modifEmpl.php
+++ vues/modifEmpl.php
@@ -1,7 +1,6 @@
 <?php
 require_once('/modele/UserDAO.class.php');
 $nom=$prenom=$email=$tel=$ville=$prov=$codePost=$adresse=$pass="";
-
  if(isset($_REQUEST["idEmp"])){
     $user= UserDAO::findById($_REQUEST["idEmp"]);
     }
@@ -10,8 +9,7 @@ $nom=$prenom=$email=$tel=$ville=$prov=$codePost=$adresse=$pass="";
     $idEmpl=$u->getId_user();
     $user= UserDAO::findById($idEmpl);    
         }
- 
-if($user!=NULL){
+ if($user!=NULL){
 $nom=$user->getNom();
 $prenom=$user->getPrenom();
 $email=$user->getCourriel();
@@ -22,7 +20,6 @@ $codePost=$user->getCode_postale();
 $adresse=$user->getAdresse();
 $pass=$user->getPassword();
 }
-
 ?>
 <div class="row">
 		<div class="col-md-12">
@@ -38,19 +35,53 @@ $pass=$user->getPassword();
 			</div>
 		</div>
 </div>
-<h1>Modifier le compte</h1>
 <form method="post">
-    <div><label for="nom">Nom</label><input required type="text" name="nomEmplMod" id="nomEmplMod" value="<?=$nom?>"></div>
-    <div><label for="prenom">Prenom</label><input required type="text" name="prenomEmpl" id="prenomEmpl" value="<?=$prenom?>"></div>
-    <div><label for="email">Courriel</label><input required type="email" name="emailEmpl" id="emailEmpl" value="<?=$email?>"></div>
-    <div><label for="tel">Téléphone</label><input required type="text" name="telEmpl" id="telEmpl" value="<?=$tel?>"></div>
-    <div><label for="ville">Ville</label><input required type="text" name="villeEmpl" id="villeEmpl" value="<?=$ville?>"></div>
-    <div><label for="province">Province</label><input required type="text" name="provEmpl" id="provEmpl" value="<?=$prov?>"></div>
-    <div><label for="code">Code postale</label><input required type="text" name="codeEmpl" id="codeEmpl" value="<?=$codePost?>"></div>
-    <div><label for="adresse">Adresse</label><input required type="text" name="adrEmpl" id="adrEmpl" value="<?=$adresse?>"></div>
-    <div><label for="password">Mot de pass</label><input required type="text" name="pasEmpl" id="pasEmpl" value="<?=$pass?>"></div>
-    <div><input type="submit" value="Sauvegarder" id="ok"></div>
-    <div><input type="button" value="Modifier" id="mod">
-    <input type="button" value="Annuler" id="annul"></div>
-<input name="action" value="save" type="hidden" />
+<div class="row" >
+	<div class="col-md-4"></div>
+	<div class="col-md-4">
+    <div class="form-group">
+		<label for="nom">Nom</label>
+		<input required type="text" name="nomEmplMod" id="nomEmplMod" value="<?=$nom?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="prenom">Prenom</label>
+		<input required type="text" name="prenomEmpl" id="prenomEmpl" value="<?=$prenom?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="email">Courriel</label>
+		<input required type="email" name="emailEmpl" id="emailEmpl" value="<?=$email?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="tel">Téléphone</label>
+		<input required type="text" name="telEmpl" id="telEmpl" value="<?=$tel?>"class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="ville">Ville</label>
+		<input required type="text" name="villeEmpl" id="villeEmpl" value="<?=$ville?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="province">Province</label>
+		<input required type="text" name="provEmpl" id="provEmpl" value="<?=$prov?>" class="form-control">
+		</div>
+    <div class="form-group">
+		<label for="code">Code postale</label>
+		<input required type="text" name="codeEmpl" id="codeEmpl" value="<?=$codePost?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="adresse">Adresse</label>
+		<input required type="text" name="adrEmpl" id="adrEmpl" value="<?=$adresse?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<label for="password">Mot de pass</label>
+		<input required type="text" name="pasEmpl" id="pasEmpl" value="<?=$pass?>" class="form-control">
+	</div>
+    <div class="form-group">
+		<input type="submit" value="Sauvegarder" id="ok" class="form-control btn btn-info "></div>
+    <div class="form-group">
+		<input type="button" value="Modifier" id="mod" class="form-control btn btn-info ">
+		<input type="button" value="Annuler" id="annul" class="form-control btn btn-danger">
+	</div>
+		<input name="action" value="save" type="hidden" />
+</div>
+</div>
 </form>
